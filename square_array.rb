bd487126e387c59441a222b5686783fb96709a77
Bobbi York <york1670@gmail.com>
2018-09-26 19:50:27 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(numbers)
   numbers = [1,2,3]
-  numbers.each do|number|**2
-    puts "The square of #{number} is #{number}"
+  numbers.each do|number|
+    square_array[] = number ** 2
+    puts square_array
   end
 end
